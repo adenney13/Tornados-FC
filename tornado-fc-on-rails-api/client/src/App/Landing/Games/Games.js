483715c8bc18f9909ee66bdffcb5422f7f5bc95e
Aaron Denney <aarondenney13@gmail.com>
2019-03-05 12:48:33 -0500

    all games seed data

--- tornado-fc-on-rails-api/client/src/App/Landing/Games/Games.js
+++ tornado-fc-on-rails-api/client/src/App/Landing/Games/Games.js
@@ -7,8 +7,8 @@ const Games = (props) => {
              {props.games.map(game => {
                 return (
                     <div>
-                    <p key={game.id}>{game.home_teams.name} vs {game.away_teams.name} @ {game.field.name} </p>
                     <p key={game.id}>Date: {game.date} Time: {game.time}</p>
+                    <p key={game.id}>{game.home_teams.name} vs {game.away_teams.name} @ {game.field.name} </p>
                     <br />
                     </div>
                 )
